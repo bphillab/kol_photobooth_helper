dfaabe2c9969501ff87bd98d87d581fbb5d374f3
Benjamin <benjamin.phillabaum@gmail.com>
2024-12-09 05:42:23 -0500

    Added in the autobreak piece of this.

--- src/Baron.ts
+++ src/Baron.ts
@@ -7,8 +7,8 @@ export function find_baron():void{
 
 export function fight_baron():void {
     const baron_location = 1 + getProperty("tavernLayout").indexOf("6");
-    equip(toItem("photo booth supply list"))
-    visitUrl( `cellar.php?action=explore&whichspot=${baron_location}` );
+    equip(toItem("photo booth supply list"));
     setProperty("choiceAdventure511","0");
+    visitUrl( `cellar.php?action=explore&whichspot=${baron_location}` );
     return;
 }
